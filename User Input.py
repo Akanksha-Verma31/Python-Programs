0221ace2b841cd8f4c58d0324784b71d4e880322
Akanksha Verma <22906.akanksha@gmail.com>
2021-08-12 21:18:22 +0530

    Create User Input.py

---
+++ User Input.py
@@ -0,0 +1,16 @@
+# How to take user input in python
+
+a = input("Enter first number : ")
+b = input("Enter second number : ")
+print(a)
+print(b)
+# This will accept all type of input i.e. it will not depend on data type
+# also it will not give any error in printing that too
+
+
+# if we want to take input of int data type
+p = int(input("Enter first number : "))
+q = int(input("Enter second number : "))
+c = a + b
+print("Sum : ", c)
+# While it will give error if we enter value other than of type 'int'
