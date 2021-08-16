d7d13e64e86f31cc10e149424d8d016e9f3b670a
Akanksha Verma <22906.akanksha@gmail.com>
2021-08-16 14:46:13 +0530

    Create Type Casting.py

---
+++ Type Casting.py
@@ -0,0 +1,7 @@
+num_int = 123
+num_float = 1.23
+num_new = num_int + num_float
+# num_new will be of type float
+print("Value of num_new : ",num_new)
+print("Type of num_new : ",type(num_new))
+# the resulted conversion is in float because float has higher precedence than that of int
