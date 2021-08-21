2ca890ddb863548bbeec2db9fcf96879cd4296c7
Akanksha Verma <22906.akanksha@gmail.com>
2021-08-21 13:56:11 +0530

    Create Take km from user and convert in into miles.py

---
+++ Take km from user and convert in into miles.py
@@ -0,0 +1,4 @@
+var1 = float(input("enter value in km: "))
+conversion = 0.621371
+miles = var1*conversion
+print("The number of miles are (0)",format(miles))    # the format functin will put the variable in the place holders place
