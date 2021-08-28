31102d215b0550a61d6cf4dce64c8eb8bad47503
Akanksha Verma <22906.akanksha@gmail.com>
2021-08-28 19:29:27 +0530

    Create user defined function.py

---
+++ user defined function.py
@@ -0,0 +1,9 @@
+# user defined functions
+
+def multiply(a,b):
+    c = a*b
+    print(f"Multiplication of {a,b} is ", c)
+
+a = int(input("Enter value 1 : "))
+b = int(input("Enter value 2 : "))
+multiply(a,b)
