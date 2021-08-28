4177354aa6b9db0efff71536a03101a3a93fea18
Akanksha Verma <22906.akanksha@gmail.com>
2021-08-28 19:30:58 +0530

    Create built in func.py

---
+++ built in func.py
@@ -0,0 +1,6 @@
+# built-in functions
+
+a = int(input("Enter value of a : "))
+b = int(input("Enter value of b : "))
+c = sum((a,b))     # using built-in function sum()
+print(f"Sum of {a} and {b} is ",c)
