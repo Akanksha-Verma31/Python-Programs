5332b7b5d987f0c4c8f5c9b45664f2b18eb7e8b1
Akanksha Verma <22906.akanksha@gmail.com>
2021-08-31 18:19:20 +0530

    Delete file handling3.py

--- file handling3.py
+++
@@ -1,9 +0,0 @@
-f = open("write.txt","r")
-print(f.readline(10))
-# prints 10 characters
-print(f.readline())
-# prints first line of the file
-print(f.readline())
-# prints second line of the file
-print(f.readline(10))
-f.close()
