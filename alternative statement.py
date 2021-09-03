f761874d4b4620b62ffa152ea3d08f0bcde501ae
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-03 11:38:37 +0530

    Create alternative statement.py

---
+++ alternative statement.py
@@ -0,0 +1,12 @@
+# ALTERNATIVE STATEMENT...
+# if-else statement
+# no. is even or odd
+x = int(input("Enter a number : "))
+if x%2 == 0:
+    print("X is even!!")
+    print("Inside if branch/block...")
+else:
+    print("X is odd!!")
+    print("Inside else branch/block...")
+    
+print("Statement outside if-else...")
