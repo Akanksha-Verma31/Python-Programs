95bc5c9c18d0d29b57b16a5e8637baedf06ef32a
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-03 11:39:18 +0530

    Create chained conditionals.py

---
+++ chained conditionals.py
@@ -0,0 +1,16 @@
+# CHAINED CONDITIONALS...
+# if-elif-else
+
+x = int(input("Enter a number: "))
+
+if x == 0:
+    print(x," is zero")
+    print("if block")
+elif x>y:
+    print(x," is greater than 0")
+    print("elif block")
+else:
+    print(x," is less than 0")
+    print("else block")
+    
+print("Outside if-elif-else...")
