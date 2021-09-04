6e565184c67f2930340ff6c7c9990006668e2c48
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-04 20:24:51 +0530

    Create while.py

---
+++ while.py
@@ -0,0 +1,11 @@
+# while loop
+# 1)initialization
+# 2)condition
+# 3)increment/decrement the value
+# infinite loop: (ctrl+c)
+# display 10 numbers using while loop
+
+i = 1
+while(i<=10):
+    print(i)
+    i+=1
