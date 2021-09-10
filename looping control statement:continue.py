01a666d981e1342b79fc97ecda5cc1251decfb33
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-10 13:26:37 +0530

    Create looping control statement:continue.py

---
+++ looping control statement:continue.py
@@ -0,0 +1,6 @@
+# looping control statement
+for i in range(1,11):
+    print(i)
+    if i == 5:
+        continue      # it will only skip that particular iteration and continue looping
+print("outside for block")
