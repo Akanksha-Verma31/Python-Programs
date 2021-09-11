736336abf19b248ce31240da3dee552df32b86bb
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-11 11:15:29 +0530

    Star Triangle

---
+++ Pattern5.py
@@ -0,0 +1,9 @@
+n = int(input("Enter range: "))
+space = n - 1
+for i in range(0,n):
+    for j in range(0,space):
+        print(end=" ")
+    space = space - 1
+    for j in range(0,i+1):
+        print("* ", end="")
+    print("\r")
