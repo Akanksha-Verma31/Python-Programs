6275a0b6a58eb8a1f9b0419b505a9988a8783cf3
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-18 18:16:12 +0530

    Create List1.py

---
+++ List1.py
@@ -0,0 +1,12 @@
+empty_list = []
+list_no = [0,2,4,6]
+list_string = ["a","b","c"]
+list_mix = [2,2.0,"a",[1,2,3,4]]
+print(list_mix[0])
+print(list_mix[1])
+print(list_mix[2])
+print(list_mix[3][2])
+print(list_mix[4-1])
+print(list_mix[:-1])
+print(list_string[-2])
+
