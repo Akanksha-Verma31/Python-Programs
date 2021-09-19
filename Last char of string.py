73bcb692a93a1f73827a56936c0828e337fccc2a
Akanksha Verma <22906.akanksha@gmail.com>
2021-09-19 13:07:42 +0530

    Create Last char of string.py

---
+++ Last char of string.py
@@ -0,0 +1,7 @@
+# to get the last letter of the string
+fruit = "banana"
+length = len(fruit)
+last = fruit[length-1]
+print("Last character: ",last)
+print(fruit[-1])   # yields last character
+print(fruit[-2])   # yields second last character
