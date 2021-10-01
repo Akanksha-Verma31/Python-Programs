f8fb4c7b3a0a467e14d0bec0cb518b143e45a7f5
Akanksha Verma <22906.akanksha@gmail.com>
2021-10-01 09:01:30 +0530

    Create inbuilt func in list.py

---
+++ inbuilt func in list.py
@@ -0,0 +1,15 @@
+# inbuilt functions in list
+a = [1,2,3,4,5,6,1]
+a.append(7)
+print(a)
+a.count(1)
+print(a.index(3))
+print(a.remove(6))
+a.reverse()
+print(a)
+a.sort()
+print(a)
+print(min(a))
+print(max(a))
+a.insert(2,90)
+print(a)
